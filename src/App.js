9d9bab35895ac0212968c4332d538248fc7c9fe9
Fernando <luisfernandosalasg@gmail.com>
2022-05-31 10:16:41 -0500

    Add Routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import Calculator from './components/Calculator';
+/* eslint-disable */
+import React from "react";
+import Calculator from "./components/Calculator";
+import Home from "./components/Home";
+import Quote from "./components/Quote";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const App = () => {
+	return (
+		<div>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/calculator" element={<Calculator />} />
+				<Route path="/quote" element={<Quote />} />
+			</Routes>
+		</div>
+	);
+};
 
-  render() {
-    return (
-      <div>
-        <Calculator />
-      </div>
-    );
-  }
-}
+export default App;
---
+++ src/components/Quote.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+import React from "react";
+
+const Quote = () => {
+	return <div>Quote</div>;
+};
+
+export default Quote;
