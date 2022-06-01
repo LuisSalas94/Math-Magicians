39b4f80b5f0cb4112fbde141b51b501f6b337a1c
Fernando <luisfernandosalasg@gmail.com>
2022-06-01 12:17:45 -0500

    Fix bug

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Calculator from './components/Calculator';
-import Home from './components/Home';
-import Quote from './components/Quote';
-import Navbar from './components/Navbar';
+/* eslint-disable */
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Calculator from "./components/Calculator";
+import Home from "./components/Home";
+import Quote from "./components/Quote";
+import Navbar from "./components/Navbar";
 
 const App = () => (
-  <div>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/calculator" element={<Calculator />} />
-      <Route path="/quote" element={<Quote />} />
-    </Routes>
-  </div>
+	<div>
+		<Navbar />
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/calculator" element={<Calculator />} />
+			<Route path="/quote" element={<Quote />} />
+		</Routes>
+	</div>
 );
 
 export default App;
